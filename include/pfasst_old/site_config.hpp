b8cf0e1b35c8249683f4f14d878bce3d5a05b658
Torbjörn Klatt <t.klatt@fz-juelich.de>
2015-06-30 10:20:34 +0200

    move old stuff away
    Signed-off-by: Torbjörn Klatt <t.klatt@fz-juelich.de>

---
+++ include/pfasst_old/site_config.hpp
@@ -0,0 +1,18 @@
+/*
+ * DO NOT ALTER THIS FILE
+ *
+ * It will get rewritten on CMake's next run
+ */
+
+#ifndef _PFASST__SITE_CONFIG_HPP
+#define _PFASST__SITE_CONFIG_HPP
+
+#include <string>
+using namespace std;
+
+namespace pfasst
+{
+  static constexpr const char* VERSION = "v0.5.0-rc1-4-g6edd80f-dirty";
+}  // ::pfasst
+
+#endif  // _PFASST__SITE_CONFIG_HPP
